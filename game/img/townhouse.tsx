ab94e969230dc27f0aff36711bd0510e8067a4f5
IoriBranford <ioribranford@gmail.com>
2017-07-03 12:27:50 -0700

    Thatched-roof cottages for villagers.

---
+++ game/img/townhouse.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="townhouse" tilewidth="16" tileheight="16" tilecount="153" columns="17">
+ <image source="townhouse.png" width="272" height="144"/>
+</tileset>
