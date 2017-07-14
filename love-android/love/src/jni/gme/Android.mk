bfb07e3e2e36a8b8ea3c0c7d7075223f0b261593
IoriBranford <ioribranford@gmail.com>
2017-07-14 01:25:01 -0700

    Keep game-music-emu at v0.6.1 in Android.

--- love-android/love/src/jni/gme/Android.mk
+++ love-android/love/src/jni/gme/Android.mk
@@ -5,5 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libgme
 FILE_LIST	:= $(wildcard $(LOCAL_PATH)/gme/*.cpp)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+# For GME after ver 0.6.1 add:
+# LOCAL_CPPFLAGS := -std=c++11
 
 include $(BUILD_SHARED_LIBRARY)
