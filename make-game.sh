3d65cbbcbfbb56feaa1da6188f141feaa25436d2
IoriBranford <ioribranford@gmail.com>
2017-07-24 17:58:06 -0700

    Validate Lua code in make-game.sh.

--- make-game.sh
+++ make-game.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
 
 PROJECT=${PWD##*/}
 GAME_ASSET=${GAME_ASSET:=game.love}
 
 cd game
+find -name "*.lua" -exec luajit -bl {} > /dev/null \;
 find -name "*.lua" | zip ${GAME_ASSET} -@
 find -name "*.png" | zip ${GAME_ASSET} -@
 find -name "*.vgm" | zip ${GAME_ASSET} -@
